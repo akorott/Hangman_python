161b09085e90ac0ff189f3d42d9224481ae8e732
Admin <akorott@gmail.com>
2020-09-29 17:36:28 -0300

    more changes

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,14 @@
 import randomword
 import time
 
+print('--------')
+print(f"|     |")
+print(f"|     O")
+print(f"|    \|/")
+print(f"|     |")
+print(f"|    / \\")
+print("-")
+
 def hanging():
     guess1 = '|'
     guess2 = 'O'
@@ -12,7 +20,6 @@ def hanging():
     guess8 = '\\'
 
     print('--------')
-    print('yoyooy')
     print(f"|     {guess1}")
     print(f"|     {guess2}")
     print(f"|    {guess4}{guess3}{guess5}")
@@ -20,30 +27,39 @@ def hanging():
     print(f"|    {guess7} {guess8}")
     print("-")
 
-print('Welcome to Hangman!')
 
-# time.sleep(1)
 print('You have 8 tries to guess the word!')
 
-print(hanging())
-
-print("Let's begin!")
-print("Please guess a letter or word:")
-
 random_word = randomword.get_random_word()
-print(random_word)
+random_word_split = list(random_word)
+
+print(random_word_split)
 blank_spaces = '_ ' * len(random_word)
 print(blank_spaces)
 
-def user_guess():
-    counter = 0
-    answer = ''
-    try:
-        guess = input()
-    except:
-        ValueError
+# def user_guess():
+#     counter = 0
+#     answer = ''
+#     try:
+#         guess = input()
+#     except:
+#         ValueError
+#
+# print(user_guess())
 
-print(user_guess())
+
+
+count = 0
+while count < 8:
+    guess = input('Guess a letter or word: ')
+    if guess in random_word_split or guess == random_word:
+        hanging(guess)
+        print('Nice!')
+    else:
+        count += 1
+        print('incorrect')
+
+print('You lose!')
 
 
 
